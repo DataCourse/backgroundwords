1c7e9251857deeef39dcec5e5e68e191e6bd10bf
Firas Wehbe <wehbefh@eto-demerzel.gateway.2wire.net>
2013-09-15 14:17:18 -0500

    First commit

---
+++ surveyanalysis.R
@@ -0,0 +1,33 @@
+# Empty memory and read the surveys 
+# (need to be downloaded from REDCap and stored input/survey.csv)
+rm(list=ls())
+mysurveys <- read.csv("input/mysurveys.csv", stringsAsFactors=FALSE)
+
+# Plot the counts of survey responses over time
+mycounts <- mysurveys$X.record_id
+mytimes <- as.POSIXct(mysurveys$presurvey_timestamp)
+png(filename="output/surveyresponse.png",width=1280,height=960,res=150)
+plot(mytimes,mycounts,type="l",ylim=c(0,4000),lwd=3,xlab="Time (UTC)",ylab="# of Survey Responses")
+abline(h=seq(0,4000,1000),lwd=1)
+abline(h=seq(500,3500,1000),lty="dashed")
+dev.off()
+
+# Word cloud
+png(filename="output/wordcloud.png",width=1200,height=1200,res=300)
+library('wordcloud')
+myexperience <- tolower(mysurveys$prev_experience)
+myexperience <- str_replace_all(myexperience,"[^[:alpha:]]"," ")
+myexperience <- str_replace_all(myexperience, "[[:blank:]]+", " ")
+allwords = data.frame(table(strsplit(paste(myexperience,collapse=" ")," ")))
+allwords <- allwords[allwords[,1]!="",]
+allwords <- allwords[!allwords[,1] %in% c('of','am','no','a','i','in',
+                                          'and','have','with','as','to',
+                                          'my','for','the','on','at','so',
+                                          'an','or','but','ve','about','had',
+                                          'did','done','do','our','is','not',
+                                          'that','this','we','by','some','any',
+                                          'it','none','from','was'),]
+wordcloud(allwords[,1],allwords[,2],scale=c(2,0.5),rot.per=0.1,max.words=300,
+          random.order=FALSE,colors=rainbow(20,start=0.25,end=1,v=0.6),random.color=TRUE)
+dev.off()
+write.csv(allwords,file="output/wordfreq.csv",row.names=FALSE)
